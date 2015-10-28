55c5a7ea41fc0fc83636ec571da504e5536c2579
Andy Schaaf <andyschaaf13@aol.com>
2015-10-28 09:49:01 -0400

    Initial commit

---
+++ RedBlocks/src/redblocks/RedBlocks.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redblocks;
+
+/**
+ *
+ * @author andy
+ */
+public class RedBlocks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LogInScreen startUp = new LogInScreen();
+        startUp.setVisible(true);
+    }
+    
+}
